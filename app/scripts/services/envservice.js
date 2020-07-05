c3b936b2cb72767feb465dfc9dcf8bfe3e341d97
rabp_ <rabp_@192.168.1.8>
2020-07-05 14:30:00 -0500

    generar datos con seed CategoriesSeed

--- app/scripts/services/envservice.js
+++ app/scripts/services/envservice.js
@@ -13,13 +13,9 @@ angular.module('sa20App')
         getHost: function() {
             switch (window.location.hostname) {
                 case 'localhost':
-                    return 'http://localhost:8000/sa20-backend/';
+                    return 'http://localhost/sa20-backend/';
                 case 'admin.inexdeo.robertobocanegra.com':
                     return 'http://inexdeo.robertobocanegra.com/api/';
-                case 'iedsa.com.pe':
-                    return 'http://iedsa.com.pe/api/';
-                case 'www.iedsa.com.pe':
-                    return 'http://iedsa.com.pe/api/';
             }
         }
     };
